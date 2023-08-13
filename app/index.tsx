946810fa028182da068c9b465f2f6c0738a3b6e0
Bolshiyanov <bolshiyanov@gmail.com>
2023-08-13 12:22:43 +0100

    Test swg icons

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import { StyleSheet } from 'react-native';
 import { Text, View } from 'react-native';
-
+import Bower from '@/src/components/logos/mug-saucer-solid.svg';
 export default function IndexScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
+      <Bower width={40} height={40} />
     </View>
   );
 }
@@ -23,5 +24,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  logo: {
+    marginTop: 20,
+    marginBottom: 20,
   },
 });
